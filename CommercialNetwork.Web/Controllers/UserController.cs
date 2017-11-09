191140f1d951b19b6691065ef269ef29c3837e9b
DenisYurchishko <d.yurchishko@andersenlab.com>
2017-11-09 22:35:05 +0300

    Проекты для логики

---
+++ CommercialNetwork.Web/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CommercialNetwork.Models.Models;
+
+namespace CommercialNetwork.Web.Controllers
+{
+    public class UserController : ApiController
+    {
+        public UserModel RegistrationUser(UserModel newUser)
+        {
+            return newUser;
+        }
+    }
+}
